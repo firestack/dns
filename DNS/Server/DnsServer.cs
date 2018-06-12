59ade1fc2069e67a954a08923b245b2c9cc8eaf0
Kayla Firestack <me@stackfire.io>
2018-06-12 03:40:10 -0600

    add other listen methods, ctrl-alt-f changed whitespace formatting

--- DNS/Server/DnsServer.cs
+++ DNS/Server/DnsServer.cs
@@ -10,100 +10,120 @@ using DNS.Protocol.Utils;
 using DNS.Client;
 using DNS.Client.RequestResolver;
 
-namespace DNS.Server {
-    public class DnsServer: IDisposable {
-        private const int DEFAULT_PORT = 53;
-        private const int UDP_TIMEOUT = 2000;
+namespace DNS.Server
+{
+	public class DnsServer : IDisposable
+	{
+		private const int DEFAULT_PORT = 53;
+		private const int UDP_TIMEOUT = 2000;
 
-        public delegate void RequestedEventHandler(IRequest request);
-        public delegate void RespondedEventHandler(IRequest request, IResponse response);
-        public delegate void ListeningEventHandler();
-        public delegate void ErroredEventHandler(Exception e);
+		public delegate void RequestedEventHandler(IRequest request);
+		public delegate void RespondedEventHandler(IRequest request, IResponse response);
+		public delegate void ListeningEventHandler();
+		public delegate void ErroredEventHandler(Exception e);
 
-        public event RequestedEventHandler Requested;
-        public event RespondedEventHandler Responded;
-        public event ListeningEventHandler Listening;
-        public event ErroredEventHandler Errored;
+		public event RequestedEventHandler Requested;
+		public event RespondedEventHandler Responded;
+		public event ListeningEventHandler Listening;
+		public event ErroredEventHandler Errored;
 
-        private bool run = true;
-        private bool disposed = false;
-        private UdpClient udp;
-        private IRequestResolver resolver;
+		private bool run = true;
+		private bool disposed = false;
+		private UdpClient udp;
+		private IRequestResolver resolver;
 
-        public DnsServer(MasterFile masterFile, IPEndPoint endServer) :
-            this(new FallbackRequestResolver(masterFile, new UdpRequestResolver(endServer))) {}
+		public DnsServer(MasterFile masterFile, IPEndPoint endServer) :
+			this(new FallbackRequestResolver(masterFile, new UdpRequestResolver(endServer)))
+		{ }
 
-        public DnsServer(MasterFile masterFile, IPAddress endServer, int port = DEFAULT_PORT) :
-            this(masterFile, new IPEndPoint(endServer, port)) {}
+		public DnsServer(MasterFile masterFile, IPAddress endServer, int port = DEFAULT_PORT) :
+			this(masterFile, new IPEndPoint(endServer, port))
+		{ }
 
-        public DnsServer(MasterFile masterFile, string endServer, int port = DEFAULT_PORT) :
-            this(masterFile, IPAddress.Parse(endServer), port) {}
+		public DnsServer(MasterFile masterFile, string endServer, int port = DEFAULT_PORT) :
+			this(masterFile, IPAddress.Parse(endServer), port)
+		{ }
 
-        public DnsServer(IPEndPoint endServer) :
-            this(new UdpRequestResolver(endServer)) {}
+		public DnsServer(IPEndPoint endServer) :
+			this(new UdpRequestResolver(endServer))
+		{ }
 
-        public DnsServer(IPAddress endServer, int port = DEFAULT_PORT) :
-            this(new IPEndPoint(endServer, port)) {}
+		public DnsServer(IPAddress endServer, int port = DEFAULT_PORT) :
+			this(new IPEndPoint(endServer, port))
+		{ }
 
-        public DnsServer(string endServer, int port = DEFAULT_PORT) :
-            this(IPAddress.Parse(endServer), port) {}
+		public DnsServer(string endServer, int port = DEFAULT_PORT) :
+			this(IPAddress.Parse(endServer), port)
+		{ }
 
-        public DnsServer(IRequestResolver resolver) {
-            this.resolver = resolver;
-        }
+		public DnsServer(IRequestResolver resolver)
+		{
+			this.resolver = resolver;
+		}
 
 		public Task Listen(int port = DEFAULT_PORT)
 		{
 			return Listen(IPAddress.Any, port);
 		}
 
-        public async Task Listen(IPAddress address, int port = DEFAULT_PORT) {
-            await Task.Yield();
+		public Task Listen(IPAddress address, int port = DEFAULT_PORT)
+		{
+			return Listen(new IPEndPoint(address, port));
+		}
 
-            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
-            IPEndPoint ip = new IPEndPoint(address, port);
+		public async Task Listen(IPEndPoint ip)
+		{
+			await Task.Yield();
 
-            if (run) {
-                try {
-                    udp = new UdpClient(ip);
-                } catch (SocketException e) {
-                    OnErrored(e);
-                    return;
-                }
-            }
+			TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
 
-
-			void receiveCallback(IAsyncResult result)
+			if (run)
 			{
-				byte[] data;
-
 				try
 				{
-					data = udp.EndReceive(result, ref ip);
-					HandleRequest(data, ip);
-				}
-				catch (ObjectDisposedException)
-				{
-					// run should already be false
-					run = false;
+
+					udp = new UdpClient(ip);
 				}
 				catch (SocketException e)
 				{
 					OnErrored(e);
+					return;
 				}
-
-				if (run) udp.BeginReceive(receiveCallback, null);
-				else tcs.SetResult(null);
 			}
 
-			udp.BeginReceive(receiveCallback, null);
-            OnListening();
-            await tcs.Task;
-        }
 
-        public void Dispose() {
-            Dispose(true);
-        }
+			async Task receiveCallback()
+			{
+				UdpReceiveResult data;
+				while (run)
+				{
+					try
+					{
+						data = await udp.ReceiveAsync();
+						HandleRequest(data.Buffer, data.RemoteEndPoint);
+					}
+					catch (ObjectDisposedException)
+					{
+						// run should already be false
+						run = false;
+					}
+					catch (SocketException e)
+					{
+						OnErrored(e);
+					}
+				}
+				tcs.SetResult(null);
+			}
+
+			var udpTask = Task.Run(receiveCallback);
+			OnListening();
+			await tcs.Task;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+		}
 
 		protected virtual void OnRequested(IRequest request) => Requested?.Invoke(request);
 
@@ -113,73 +133,85 @@ namespace DNS.Server {
 
 		protected virtual void OnErrored(Exception e) => Errored?.Invoke(e);
 
-		protected virtual void Dispose(bool disposing) {
-            if (!disposed) {
-                disposed = true;
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposed)
+			{
+				disposed = true;
 
-                if (disposing) {
-                    run = false;
-                    udp?.Dispose();
-                }
-            }
-        }
+				if (disposing)
+				{
+					run = false;
+					udp?.Dispose();
+				}
+			}
+		}
 
-        private async void HandleRequest(byte[] data, IPEndPoint remote) {
-            Request request = null;
+		private async void HandleRequest(byte[] data, IPEndPoint remote)
+		{
+			Request request = null;
 
-            try {
-                request = Request.FromArray(data);
+			try
+			{
+				request = Request.FromArray(data);
 				request.Sender = remote;
-                OnRequested(request);
+				OnRequested(request);
 
-                IResponse response = await resolver.Resolve(request);
+				IResponse response = await resolver.Resolve(request);
 
-                OnResponded(request, response);
-                await udp
-                    .SendAsync(response.ToArray(), response.Size, remote)
-                    .WithCancellationTimeout(UDP_TIMEOUT);
-            }
-            catch (SocketException e) { OnErrored(e); }
-            catch (ArgumentException e) { OnErrored(e); }
-            catch (IndexOutOfRangeException e) { OnErrored(e); }
-            catch (OperationCanceledException e) { OnErrored(e); }
-            catch (IOException e) { OnErrored(e); }
-            catch (ObjectDisposedException e) { OnErrored(e); }
-            catch (ResponseException e) {
-                IResponse response = e.Response;
+				OnResponded(request, response);
+				await udp
+					.SendAsync(response.ToArray(), response.Size, remote)
+					.WithCancellationTimeout(UDP_TIMEOUT);
+			}
+			catch (SocketException e) { OnErrored(e); }
+			catch (ArgumentException e) { OnErrored(e); }
+			catch (IndexOutOfRangeException e) { OnErrored(e); }
+			catch (OperationCanceledException e) { OnErrored(e); }
+			catch (IOException e) { OnErrored(e); }
+			catch (ObjectDisposedException e) { OnErrored(e); }
+			catch (ResponseException e)
+			{
+				IResponse response = e.Response;
 
-                if (response == null) {
-                    response = Response.FromRequest(request);
-                }
+				if (response == null)
+				{
+					response = Response.FromRequest(request);
+				}
 
-                try {
-                    await udp
-                        .SendAsync(response.ToArray(), response.Size, remote)
-                        .WithCancellationTimeout(UDP_TIMEOUT);
-                }
-                catch (SocketException) {}
-                catch (OperationCanceledException) {}
-                finally { OnErrored(e); }
-            }
-        }
+				try
+				{
+					await udp
+						.SendAsync(response.ToArray(), response.Size, remote)
+						.WithCancellationTimeout(UDP_TIMEOUT);
+				}
+				catch (SocketException) { }
+				catch (OperationCanceledException) { }
+				finally { OnErrored(e); }
+			}
+		}
 
-        private class FallbackRequestResolver : IRequestResolver {
-            private IRequestResolver[] resolvers;
+		private class FallbackRequestResolver : IRequestResolver
+		{
+			private IRequestResolver[] resolvers;
 
-            public FallbackRequestResolver(params IRequestResolver[] resolvers) {
-                this.resolvers = resolvers;
-            }
+			public FallbackRequestResolver(params IRequestResolver[] resolvers)
+			{
+				this.resolvers = resolvers;
+			}
 
-            public async Task<IResponse> Resolve(IRequest request) {
-                IResponse response = null;
+			public async Task<IResponse> Resolve(IRequest request)
+			{
+				IResponse response = null;
 
-                foreach (IRequestResolver resolver in resolvers) {
-                    response = await resolver.Resolve(request);
-                    if (response.AnswerRecords.Count > 0) break;
-                }
+				foreach (IRequestResolver resolver in resolvers)
+				{
+					response = await resolver.Resolve(request);
+					if (response.AnswerRecords.Count > 0) break;
+				}
 
-                return response;
-            }
-        }
-    }
+				return response;
+			}
+		}
+	}
 }
